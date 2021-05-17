75e7c463a1a3ed004353c62a937b49eeaa70539f
JeremKeith <1055292595@qq.com>
2021-05-17 10:11:26 +0800

    fix: other

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,22 +1,24 @@
 import { SearchPanel } from "./search-pannel"
 import { List } from "./list"
+import { useEffect, useState } from "react"
+const apiURL = process.env.REACT_APP_API_URL
 export const ProjectListScreen = () => {
   const [param, setParam] = useState({
     name: '',
     personId: ''
   })
+  const [users, setUsers] = useState([])
   const [list, setList] = useState([])
   useEffect(() => {
-    fetch('').then(
-      res => {
-        if (res.ok) {
-          setList(res.json())
-        }
-      }
-    )
+    fetch(`${apiURL}/projects?name=${param.name}&personId=${param.personId}`).then(res => res.json())
+    .then(data => setList(data))
   }, [param])
+  useEffect(() => {
+    fetch(`${apiURL}/users`).then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
   return <div>
-    <SearchPannel param={param} setParam={setParam}></SearchPannel>
-    <List list={list}></List>
+    <SearchPanel users={users} param={param} setParam={setParam}></SearchPanel>
+    <List list={list} users={users}></List>
   </div>
 }
