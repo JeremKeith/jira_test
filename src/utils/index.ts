97fc28e3475ebb9ce925a7862747109ed400e22e
JeremKeith <1055292595@qq.com>
2021-07-14 14:27:10 +0800

    feat: 添加了form表单中间件

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { isTemplateExpression } from "typescript";
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 export const cleanObject = (object: object) => {
   const result = { ...object };
@@ -24,4 +25,20 @@ export const useDebounce = <T>(value: T, delay: number) => {
     return () => clearTimeout(timeout);
   }, [value, delay]);
   return debouncedValue;
+};
+export const useArray = <T>(param: T[]) => {
+  // hello，请把作业写在这里吧，写完记得再对照作业要求检查一下
+  const [value, setValue] = useState(param);
+  const add = (item: T) => {
+    setValue([...value, item]);
+  };
+  const removeIndex = (num: number) => {
+    const tmp = [...value];
+    tmp.splice(num, 1);
+    setValue(tmp);
+  };
+  const clear = () => {
+    setValue([]);
+  };
+  return { value, setValue, add, clear, removeIndex };
 };
